f12de1ca88ccb09f0e3a025f7560769665f02991
HyperSkys <thebestdude112@gmail.com>
2023-06-08 03:18:20 -0400

    github pen

---
+++ src/main/java/dev/hyperskys/mugrootbear/Main.java
@@ -0,0 +1,151 @@
+                                                        package
+        dev.
+ hyperskys
+                .mugrootbear;
+import                          java.
+                                                            security.
+        SecureRandom;
+public                                         class Main{
+public           static void main(String[]args)
+                                               throws InterruptedException
+{
+System                      .out.println            ("[Mug]: welcum to the mug" +
+                                    " rootbear facility!") ;
+            Thread.sleep(             1000) ;
+System                .out. println                           ("[Mug]: we are " +
+                                                                                                 "currently in the process " +
+        "of making mug rootbear!"        ) ;
+          int ix81ms            =new             SecureRandom( ).                nextInt( 1,15);
+for(int i =                                              0;i<ix81ms                          ;i++){
+               Thread.             sleep(1000);
+         System.                                    out.println                           ("[Mug]: making mug rootbear");
+        }
+                                          Thread.                                                  sleep(1000);
+                             System              .out.                        println             ("[Mug]: " +
+                                                                                                              "we made the mug rootbear " +
+                                     "woooooah!");
+            Thread.                               sleep(500
+            );
+System.                             out.             println               ("[Mug]: we are now " +
+        "going to make the mug rootbear " +
+                                                                                                "fizzy!");
+                                         Thread.sleep                                                 (50)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                                                                                            System.out.println            ("[Mug]: " +
+        "we are " +
+        "now selling your data to the chinese government");
+                                         Thread                                                               .sleep (
+                                                 50);
+                        System           .out.
+                                                                                                 println("[Mug]:" +
+                                                                      "we are now making the m" +
+                                                    "ug rootbear with male gen" +
+                                                "itals");
+         Thread.
+               sleep                  (
+                            50
+                                                      );
+                                                    System.
+           out.
+                                                            println("[Mug]: woops" +
+                                                                                                " your dicks go" +
+                                                                                                                   "ne woooooah");
+                            Thread.
+           sleep(
+                                                                                        50);
+                                                                              System.out.println("[Mug]: " +
+                                                        "im gonna say a bad offensive word right " +
+                                 "now");
+                 Thread.
+                  sleep        (
+                          50
+                                );
+                                                System.out.
+                                                        println("" +
+                                                                     "" +
+                                                                               "" +
+                                                                "" +
+                                                                                    "" +
+                                                                "[Mug]: " +
+                                                                                                        "N");
+                                                                                             Thread.
+                                                                                                     sleep
+                                                         (50
+                                                                                                             );
+                        System . out .
+                                println("[Mug]: " +
+                                        "" +
+                                         "" +
+                                         "" +
+                                         "" +
+                                                   "" +
+                                             "" +
+                                           "" +
+                                           "" +
+                         "I");
+                                                                     Thread.sleep
+
+                                                (                      50
+                                                )
+                                                                     ;
+                                                                             System.
+                                                                                     out.println("" +
+                                                                         "[Mu" +
+                                                                                             "g]" +
+                                                                                    ": " +
+                                                                                             "G");
+                             Thread.
+                                     sleep(
+50
+                             );
+             System.out.
+                     println("[" +
+                                                             "Mug]: " +
+                     "G");
+                                                         Thread
+                                                                 .sleep(50)
+                                                         ;;;;;;;;;;;;;;;;;;;;;;
+                                                         ;
+                                 System.
+                                         out.
+                                            println
+                                                        ("[Mu" +
+                                                                "g]: L");
+         Thread
+                                                                           .sleep
+                         (50)
+         ;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                                       System.out.
+                                                               println(
+                                                                       "[Mug" +
+                             "" +
+                                                                    "" +
+                                        "]: " +
+                                                                                              "" +
+                                                                      "" +
+       "E");
+                                                   }
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                                                };;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
